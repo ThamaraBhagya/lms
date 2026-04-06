33411d5b72396877d8ffce49d804ee9768353e10
Thamara Bhagya <thamarabhagya755@gmail.com>
2026-04-06 17:51:54 +0530

    resolve proxy

--- eduflow-frontend/next.config.ts
+++ eduflow-frontend/next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        // This catches everything starting with /api
-        source: '/api/:path*',
-        // This ADDS /api back to the beginning of the AWS call
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`, 
-      },
-    ];
-  },
+  
 };
 
 export default nextConfig;
